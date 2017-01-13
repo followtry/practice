7c0eb6f90453931fb0399343d18a808b41f6ace0
followtry <zhongzhijing@126.com>
2017-01-13 15:11:39 +0800

    封装获取系统属性的类； 包更名； 孵化模块练习； brief-tools增加对方法级别的日志监控

---
+++ 清理工程配置文件.bat
@@ -0,0 +1,10 @@
+del /a /f /s /q *.project
+del /a /f /s /q *.classpath
+del /a /f /s /q *.prefs
+del /a /f /s /q *.component
+del /a /f /s /q *.project.facet.core.xml
+del /a /f /s /q *.prefs
+del /a /f /s /q *.container
+del /a /f /s /q *.superType.name
+del /a /f /s /q *dtscope
+pause
